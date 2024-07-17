d72a8d435d0e4d2060268961e02f83022bf38414
tolmachev-pravo <d.tolmachev@pravo.tech>
2024-07-17 17:19:18 +0400

    Add "In" tests

---
+++ DbQueryTextBuilder.Tests/MSSql/InTests.cs
@@ -0,0 +1,31 @@
+﻿namespace DbQueryTextBuilder.Tests.MSSql
+{
+	[TestFixture]
+	public class InTests
+	{
+		private IDbQueryTextBuilder _builder;
+
+		[SetUp]
+		public void Setup()
+		{
+			_builder = new MSSqlDbQueryTextBuilder();
+		}
+
+		[Test]
+		public void InGuidValues_Should_BeCorrect()
+		{
+			var values = new List<Guid>
+			{
+				Guid.NewGuid(),
+				Guid.NewGuid()
+			};
+
+			var query = _builder
+				.Column("table1", "column1")
+				.In(values)
+				.ToString();
+
+			Assert.That(query?.ToOneLine(), Is.EqualTo($"{"table1".Quote()}.{"column1".Quote()} IN ('{values[0]}', '{values[1]}')"));
+		}
+	}
+}
---
+++ DbQueryTextBuilder.Tests/PostgreSql/InTests.cs
@@ -0,0 +1,31 @@
+﻿namespace DbQueryTextBuilder.Tests.PostgreSql
+{
+	[TestFixture]
+	public class InTests
+	{
+		private IDbQueryTextBuilder _builder;
+
+		[SetUp]
+		public void Setup()
+		{
+			_builder = new PostgreSqlDbQueryTextBuilder();
+		}
+
+		[Test]
+		public void InGuidValues_Should_BeCorrect()
+		{
+			var values = new List<Guid>
+			{
+				Guid.NewGuid(),
+				Guid.NewGuid()
+			};
+
+			var query = _builder
+				.Column("table1", "column1")
+				.In(values)
+				.ToString();
+
+			Assert.That(query?.ToOneLine(), Is.EqualTo($"{"table1".Quote()}.{"column1".Quote()} IN (CAST('{values[0]}' AS uuid), CAST('{values[1]}' AS uuid))"));
+		}
+	}
+}
